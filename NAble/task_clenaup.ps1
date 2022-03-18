7b52e0249e31174b591cfea9dbe01ca6509aee83
CJ <khristos666@gmail.com>
2022-03-18 17:12:16 -0400

    Create task_clenaup.ps1

---
+++ NAble/task_clenaup.ps1
@@ -0,0 +1,60 @@
+#REGION ----- DECLARATIONS ----
+  $taskFOL = "\IPM Computers\"
+  $tasks = @("N-able Windows Agent Self-Healing", "NcentralAMX")
+#ENDREGION ----- DECLARATIONS ----
+
+#REGION ----- FUNCTIONS ----
+  function Get-ProcessOutput {
+    Param (
+      [Parameter(Mandatory=$true)]$FileName,
+      $Args
+    )
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo.WindowStyle = "Hidden"
+    $process.StartInfo.CreateNoWindow = $true
+    $process.StartInfo.UseShellExecute = $false
+    $process.StartInfo.RedirectStandardOutput = $true
+    $process.StartInfo.RedirectStandardError = $true
+    $process.StartInfo.FileName = $FileName
+    if($Args) {$process.StartInfo.Arguments = $Args}
+    $out = $process.Start()
+
+    $StandardError = $process.StandardError.ReadToEnd()
+    $StandardOutput = $process.StandardOutput.ReadToEnd()
+
+    $output = New-Object PSObject
+    $output | Add-Member -type NoteProperty -name StandardOutput -Value $StandardOutput
+    $output | Add-Member -type NoteProperty -name StandardError -Value $StandardError
+    return $output
+  } ## Get-ProcessOutput
+#ENDREGION ----- FUNCTIONS ----
+
+#------------
+#BEGIN SCRIPT
+foreach ($task in $tasks) {
+  try {
+    if (Get-ScheduledTask -TaskName "$($task)" -erroraction silentlycontinue) { write-host "Deleting Task : $($task)" }
+    try {
+      Unregister-ScheduledTask -TaskName "$($task)" -Confirm:$false -erroraction silentlycontinue
+      remove-item "C:\Windows\System32\Tasks$($taskFOL)" -force -erroraction silentlycontinue
+    } catch {
+      try {
+        Unregister-ScheduledTask -TaskPath "$($taskFOL)" -TaskName "$($task)" -Confirm:$false -erroraction silentlycontinue
+        remove-item "C:\Windows\System32\Tasks$($taskFOL)" -force -erroraction silentlycontinue
+      } catch {
+        write-hot "Couldn't remove task : $($task)`r`n"
+      }
+    }
+  } catch {
+    write-host "Error - Task : $($task) still present"
+  }
+  $xmltasks = Get-ChildItem -Path 'C:\Windows\System32\Tasks' | where-object { $_.Name -match "$($task)" }
+  if (!$xmltasks) {
+    write-host "Confirmed Task $($task) deleted"
+  } elseif ($xmltasks) {
+    write-host "Error - Task : $($task) still present"
+  }
+  #schtasks /Delete /TN "<task folder path>\<task name>" /F
+}
+#END SCRIPT
+#------------
