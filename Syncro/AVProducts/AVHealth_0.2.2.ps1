e504497042362a9086a68fb41f97f2bbc3de3043
CJ <khristos666@gmail.com>
2022-04-04 15:39:17 -0400

    Update AVHealth_0.2.2.ps1

--- Syncro/AVProducts/AVHealth_0.2.2.ps1
+++ Syncro/AVProducts/AVHealth_0.2.2.ps1
@@ -515,10 +515,30 @@
       9999 {return "No detections"}
     }
   } ## SEP-Map
+
+  function StopClock {
+    #Stop script execution time calculation
+    $script:sw.Stop()
+    $Days = $sw.Elapsed.Days
+    $Hours = $sw.Elapsed.Hours
+    $Minutes = $sw.Elapsed.Minutes
+    $Seconds = $sw.Elapsed.Seconds
+    $Milliseconds = $sw.Elapsed.Milliseconds
+    $ScriptStopTime = (Get-Date).ToString('dd-MM-yyyy hh:mm:ss')
+    $total = ((((($Hours * 60) + $Minutes) * 60) + $Seconds) * 1000) + $Milliseconds
+    $mill = [string]($total / 1000)
+    $mill = $mill.split(".")[1]
+    $mill = $mill.SubString(0,[math]::min(3,$mill.length))
+    $script:diag += "`r`nTotal Execution Time - $($Minutes) Minutes : $($Seconds) Seconds : $($Milliseconds) Milliseconds`r`n"
+    write-host "`r`nTotal Execution Time - $($Minutes) Minutes : $($Seconds) Seconds : $($Milliseconds) Milliseconds`r`n"
+  }
 #ENDREGION ----- FUNCTIONS ----
 
 #------------
 #BEGIN SCRIPT
+#Start script execution time calculation
+$ScrptStartTime = (Get-Date).ToString('dd-MM-yyyy hh:mm:ss')
+$script:sw = [Diagnostics.Stopwatch]::StartNew()
 Get-OSArch
 Get-AVXML $i_PAV $script:pavkey
 if (-not ($script:blnAVXML)) {
@@ -1545,6 +1565,8 @@ write-host "Competitor State :" -foregroundcolor yellow
 write-host "$($script:o_CompState)" -foregroundcolor $ccode
 $script:diag += "`r`nCompetitor AV :`r`nAV Conflict : $($script:o_AVcon)`r`n$($script:o_CompAV)"
 $script:diag += "`r`nCompetitor Path :`r`n$($script:o_CompPath)`r`nCompetitor State :`r`n$($script:o_CompState)"
+#Stop script execution time calculation
+StopClock
 #SYNCRO OUTPUT
 if ($script:blnWARN) {
   # This creates an alert in Syncro and triggers the "New RMM Alert" in the Notification Center - automatically de-duping per asset.
