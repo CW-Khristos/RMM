71dea41490ae3556285d2d43977d09f35c76ce8e
CJ <khristos666@gmail.com>
2022-04-18 09:50:30 -0400

    Update NAble_Backup_Sync_Status_v.0.1.0.ps1

--- Syncro/NAble Backups/NAble_Backup_Sync_Status_v.0.1.0.ps1
+++ Syncro/NAble Backups/NAble_Backup_Sync_Status_v.0.1.0.ps1
@@ -108,35 +108,6 @@
       $script:lsv = "LSV Sync : N/A"
     }
   }
-
-  function Split-StringOnLiteralString {
-    trap {
-      Write-Error "An error occurred using the Split-StringOnLiteralString function. This was most likely caused by the arguments supplied not being strings"
-    }
-
-    if ($args.Length -ne 2) {
-      Write-Error "Split-StringOnLiteralString was called without supplying two arguments. The first argument should be the string to be split, and the second should be the string or character on which to split the string."
-    } else {
-      if (($args[0]).GetType().Name -ne "String") {
-        Write-Warning "The first argument supplied to Split-StringOnLiteralString was not a string. It will be attempted to be converted to a string. To avoid this warning, cast arguments to a string before calling Split-StringOnLiteralString."
-        $strToSplit = [string]$args[0]
-      } else {
-        $strToSplit = $args[0]
-      }
-
-      if ((($args[1]).GetType().Name -ne "String") -and (($args[1]).GetType().Name -ne "Char")) {
-        Write-Warning "The second argument supplied to Split-StringOnLiteralString was not a string. It will be attempted to be converted to a string. To avoid this warning, cast arguments to a string before calling Split-StringOnLiteralString."
-        $strSplitter = [string]$args[1]
-      } elseif (($args[1]).GetType().Name -eq "Char") {
-        $strSplitter = [string]$args[1]
-      } else {
-        $strSplitter = $args[1]
-      }
-
-      $strSplitterInRegEx = [regex]::Escape($strSplitter)
-      [regex]::Split($strToSplit, $strSplitterInRegEx)
-    }
-  }
 #ENDREGION ----- FUNCTIONS ----
 
 #------------
@@ -192,8 +163,8 @@ if ($true_path) {
     #Retrieve the LSV Location from ClientTool
     $test = & cmd.exe /c `"$CLI_path`" control.setting.list
     $test = [String]$test
-    $items = Split-StringOnLiteralString $test "LocalSpeedVaultLocation "
-    $items = Split-StringOnLiteralString $items[1] "LocalSpeedVaultPassword "
+    $items = $test -split "LocalSpeedVaultLocation "
+    $items = $items[1] -split "LocalSpeedVaultPassword "
     $LSV_Location = $items[0]
   }
   #Check Remote & LSV Sync Status
