f5dd38e28b9db3970365d33e8f563b61ca76b00e
CJ <khristos666@gmail.com>
2023-06-08 15:15:17 -0400

    Update MonitorRogueDHCP.ps1

--- Datto/MonitorRogueDHCP.ps1
+++ Datto/MonitorRogueDHCP.ps1
@@ -1,5 +1,6 @@
 #REGION ----- DECLARATIONS ----
   $blnADD = $true
+  $blnWARN = $false
   $ListedDHCPServers = @{}
   #SET TLS SECURITY FOR CONNECTING TO GITHUB
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
@@ -12,11 +13,11 @@
     write-host "<-End Diagnostic->"
   } ## write-DRMMDiag
 
-  function write-DRRMAlert ($message) {
+  function write-DRMMAlert ($message) {
       write-host "<-Start Result->"
       write-host "Alert=$($message)"
       write-host "<-End Result->"
-  } ## write-DRRMAlert
+  } ## write-DRMMAlert
 #ENDREGION ----- FUNCTIONS ----
 
 #------------
@@ -29,11 +30,13 @@ if ($Version -lt "6.3") {
 
 try {
   $AllowedDHCPServer = Get-DhcpServerInDC
-  write-host "Local Server (In AD) : $($AllowedDHCPServer)"
+  write-host "Local Server (In AD) : "
+  $AllowedDHCPServer
 } catch {
   $ipV4 = Test-Connection -ComputerName "$($env:computername)" -Count 1  | Select -ExpandProperty IPV4Address 
   $AllowedDHCPServer = $ipV4.IPAddressToString
-  write-host "Local Server (Local IP) : $($AllowedDHCPServer)"
+  write-host "Local Server (Local IP) :"
+  $AllowedDHCPServer
 }
 
 #Replace the Download URL to where you've uploaded the DHCPTest file yourself. We will only download this file once. 
@@ -43,10 +46,9 @@ try {
   $TestDownloadLocation = Test-Path $DownloadLocation
   if (!$TestDownloadLocation) {new-item $DownloadLocation -ItemType Directory -force}
   $TestDownloadLocationZip = Test-Path "$($DownloadLocation)\DHCPTest.exe"
-  #IPM-Khristos
   if (!$TestDownloadLocationZip) {Invoke-WebRequest -UseBasicParsing -Uri $DownloadURL -OutFile "$($DownloadLocation)\DHCPTest.exe"}
 } catch {
-  write-DRRMAlert "The download and extraction of DHCPTest failed. Error: $($_.Exception.Message)"
+  write-DRMMAlert "The download and extraction of DHCPTest failed. Error: $($_.Exception.Message)"
   exit 1
 }
 $Tests = 0
@@ -54,6 +56,8 @@ $FoundServers = do {
   & "$($DownloadLocation)\DHCPTest.exe" --quiet --query --print-only 54 --wait --timeout 3
   $Tests ++
 } while ($Tests -lt 2)
+write-host "`r`nDHCP SERVERS FOUND (via DHCPTest) :"
+$FoundServers
 
 #ENSURE ONLY UNIQUE SERVERS ARE IN '$ListedDHCPServers' HASHTABLE
 foreach ($server in $FoundServers) {
@@ -72,21 +76,26 @@ foreach ($server in $FoundServers) {
     }
   }
 }
-write-host "`r`nDHCP SERVERS FOUND :"
+write-host "`r`nDHCP SERVERS TO CHECK :"
 $ListedDHCPServers.values
 
-$DHCPHealth = foreach ($ListedServer in $ListedDHCPServers.keys) {
-  write-host "`r`nCHECK SERVER : $($ListedServer)"
-  if ($AllowedDHCPServer.IPAddress -notcontains $ListedServer.value) {"Rogue DHCP Server found. IP of rogue server is $($ListedServer)"}
+$DHCPHealth = foreach ($ListedServer in $ListedDHCPServers.values) {
+  write-host "`r`nCHECK SERVER : "
+  $ListedServer
+  if ($AllowedDHCPServer.IPAddress -notcontains $ListedServer) {
+    $blnWARN = $true
+    write-host "Rogue DHCP Server found. IP of rogue server is $($ListedServer)"
+  }
 }
+$DHCPHealth = $DHCPHealth | out-string
 
-if (!$DHCPHealth) { 
-  write-DRRMAlert "Healthy. No Rogue DHCP servers found."
+if (-not $blnWARN) { 
+  write-DRMMAlert "Healthy. No Rogue DHCP servers found."
   exit 0
-} elseif ($DHCPHealth) { 
-  write-DRRMAlert $DHCPHealth
-  foreach ($ListedServer in $ListedDHCPServers.keys) {
-    if ($AllowedDHCPServer.IPAddress -notcontains $ListedServer.value) {write-DRMMDiag $ListedDHCPServers.values}
+} elseif ($blnWARN) { 
+  write-DRMMAlert $DHCPHealth
+  foreach ($ListedServer in $ListedDHCPServers.values) {
+    if ($AllowedDHCPServer.IPAddress -notcontains $ListedServer) {write-DRMMDiag $ListedServer}
   }
   exit 1
 }
