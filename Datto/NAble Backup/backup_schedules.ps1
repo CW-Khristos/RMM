b7e0024da41ec3e82eb33d48c1692ea61d03ed58
CJ <khristos666@gmail.com>
2023-08-02 16:13:48 -0400

    Update backup_schedules.ps1

--- Datto/NAble Backup/backup_schedules.ps1
+++ Datto/NAble Backup/backup_schedules.ps1
@@ -12,6 +12,13 @@
   $script:diag      = $null
   $script:blnWARN   = $false
   $script:blnBREAK  = $false
+  $hashMsg          = $null
+  $curArchives      = $env:UDF_19
+  $hashArchives     = $env:UDF_20
+  $udfArchives      = $env:udfArchives
+  $curSchedules     = $env:UDF_17
+  $hashSchedules    = $env:UDF_18
+  $udfSchedules     = $env:udfSchedules
   $strLineSeparator = "---------"
 #endregion ----- DECLARATIONS ----
 
@@ -69,6 +76,10 @@
 #BEGIN SCRIPT
 $i = -1
 clear-host
+write-host "Cached Archive Hash (UDF20) : $($hashArchives)"
+$script:diag += "Cached Archive Hash (UDF20) : $($hashArchives)`r`n"
+write-host "Cached Schedule Hash (UDF18) : $($hashSchedules)`r`n"
+$script:diag += "Cached Schedule Hash (UDF18) : $($hashSchedules)`r`n`r`n"
 cd "C:\Program Files\Backup Manager"
 #Start script execution time calculation
 $ScrptStartTime = (Get-Date).ToString('dd-MM-yyyy hh:mm:ss')
@@ -100,7 +111,27 @@ try {
   foreach ($line in $array) {write-host "`t$($line)"; $script:diag += "`t$($line)`r`n"}
   write-host "$($strLineSeparator)"
   $script:diag += "$($strLineSeparator)`r`n"
-  new-itemproperty -path "HKLM:\Software\Centrastage" -name "Custom$($env:udfSchedules)" -value "$($scheduleset)" -force
+  if (($null -eq $curSchedules) -or ($curSchedules -eq "") -or (-not $curSchedules)) {
+    new-itemproperty -path "HKLM:\Software\Centrastage" -name "Custom$($udfSchedules)" -value "$($scheduleset)" -force
+  }
+  #COMPUTE SCHEDULE HASH
+  $utf8 = new-object -TypeName System.Text.UTF8Encoding
+  $md5 = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+  $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($scheduleset)))
+  write-host "$($strLineSeparator)`r`nCOMPUTED SCHEDULE HASH :`r`n$($strLineSeparator)`r`n`t$($hash)`r`n$($strLineSeparator)"
+  $script:diag += "$($strLineSeparator)`r`nCOMPUTED SCHEDULE HASH :`r`n$($strLineSeparator)`r`n`t$($hash)`r`n$($strLineSeparator)`r`n"
+  #COMPARE SCHEDULE HASH
+  if ($hashSchedules) {
+    if (Compare-Object -ReferenceObject $hashSchedules -DifferenceObject $hash) {
+      $scheduleMsg = "| Schedule Hashes are different |"
+    } else {
+      $scheduleMsg = "| Schedule Hashes are same |"
+    }
+    write-host "`t$($scheduleMsg)`r`n$($strLineSeparator)`r`nPREV SCHEDULE :`r`n$($strLineSeparator)`r`n`t$($curSchedules)`r`n$($strLineSeparator)"
+    $script:diag += "`t$($scheduleMsg)`r`n$($strLineSeparator)`r`nPREV SCHEDULE :`r`n$($strLineSeparator)`r`n`t$($curSchedules)`r`n$($strLineSeparator)`r`n"
+  } elseif (-not $hashSchedules) {
+    new-itemproperty -path "HKLM:\Software\Centrastage" -name "Custom18" -value "$($hash)" -force
+  }
 } catch {
   $script:blnWARN = $true
   $err = "$($_.scriptstacktrace)`r`n$($_.Exception)`r`n$($_)`r`n"
@@ -116,14 +147,14 @@ try {
   $script:diag += "$($strLineSeparator)`r`nARCHIVE :`r`n$($strLineSeparator)`r`n"
   foreach ($line in $array) {write-host "`t$($line)"; $script:diag += "`t$($line)`r`n"}
   write-host "$($strLineSeparator)"
-  $script:diag += "`r`n$($strLineSeparator)"
+  $script:diag += "`r`n$($strLineSeparator)`r`n"
   #$chunk = $archive.split(" ", [StringSplitOptions]::RemoveEmptyEntries)
   #write-host "$($strLineSeparator)`r`nARRAY SPLIT :`r`n$($strLineSeparator)"
   #$script:diag += "$($strLineSeparator)`r`nARRAY SPLIT :`r`n$($strLineSeparator)`r`n"
   #foreach ($item in $chunk) {write-host "`t$($item)"; $script:diag += "`t$($item)`r`n"}
   foreach ($line in $array) {
     $chunk = $line.split(" ", [StringSplitOptions]::RemoveEmptyEntries)
-    $scheduleset += "$($chunk[2]) - $($chunk[4]) - Datasources : $($chunk[5]) - Archive Time : $($chunk[6]) - Archive Months : $($chunk[7]) - Archive Days : $($chunk[8]) | `r`n"
+    $archiveset += "$($chunk[2]) - $($chunk[4]) - Datasources : $($chunk[5]) - Archive Time : $($chunk[6]) - Archive Months : $($chunk[7]) - Archive Days : $($chunk[8]) | `r`n"
   }
   $archiveset = "$($chunk[2]) - $($chunk[4]) - Datasources : $($chunk[5]) - Archive Time : $($chunk[6]) - Archive Months : $($chunk[7]) - Archive Days : $($chunk[8])"
   $archiveset = $archiveset.replace("FileSystem","FS").replace("NetworkShares","NS").replace("SystemState","SS").replace("Exchange","EXCH").replace("VMWare","VM").replace("HyperV","HV")
@@ -134,7 +165,27 @@ try {
   foreach ($line in $array) {write-host "`t$($line)"; $script:diag += "`t$($line)`r`n"}
   write-host "$($strLineSeparator)"
   $script:diag += "$($strLineSeparator)`r`n"
-  new-itemproperty -path "HKLM:\Software\Centrastage" -name "Custom$($env:udfArchives)" -value "$($archiveset)" -force
+  if (($null -eq $curArchives) -or ($curArchives -eq "") -or (-not $curArchives)) {
+    new-itemproperty -path "HKLM:\Software\Centrastage" -name "Custom$($udfArchives)" -value "$($archiveset)" -force
+  }
+  #COMPUTE ARCHIVE HASH
+  $utf8 = new-object -TypeName System.Text.UTF8Encoding
+  $md5 = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+  $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($archiveset)))
+  write-host "$($strLineSeparator)`r`nCOMPUTED ARCHIVE HASH :`r`n$($strLineSeparator)`r`n`t$($hash)`r`n$($strLineSeparator)"
+  $script:diag += "$($strLineSeparator)`r`nCOMPUTED ARCHIVE HASH :`r`n$($strLineSeparator)`r`n`t$($hash)`r`n$($strLineSeparator)`r`n"
+  #COMPARE ARCHIVE HASH
+  if ($hashArchives) {
+    if (Compare-Object -ReferenceObject $hashArchives -DifferenceObject $hash) {
+      $archiveMsg = "| Archive Hashes are different |"
+    } else {
+      $archiveMsg = "| Archive Hashes are same |"
+    }
+    write-host "`t$($archiveMsg)`r`n$($strLineSeparator)`r`nPREV ARCHIVE :`r`n$($strLineSeparator)`r`n`t$($curArchives)`r`n$($strLineSeparator)"
+    $script:diag += "`t$($archiveMsg)`r`n$($strLineSeparator)`r`nPREV ARCHIVE :`r`n$($strLineSeparator)`r`n`t$($curArchives)`r`n$($strLineSeparator)`r`n"
+  } elseif (-not $hashArchives) {
+    new-itemproperty -path "HKLM:\Software\Centrastage" -name "Custom20" -value "$($hash)" -force
+  }
 } catch {
   $script:blnWARN = $true
   $err = "$($_.scriptstacktrace)`r`n$($_.Exception)`r`n$($_)`r`n"
@@ -144,12 +195,19 @@ try {
 #Stop script execution time calculation
 StopClock
 #DATTO OUTPUT
+$finish = "$((Get-Date).ToString('yyyy-MM-dd hh:mm:ss'))"
+if (($scheduleMsg -match "Hashes are same") -and ($archiveMsg -match "Hashes are same")) {
+  $hashMsg = "All Hashes Match"
+} elseif (($scheduleMsg -match "Hashes are different") -or ($archiveMsg -match "Hashes are different")) {
+  $hashMsg = "Detected Hash Change"
+  $script:blnWARN = $true
+}
 if ($script:blnWARN) {
-  write-DRMMAlert "Backup_Schedules : Execution Completed with Warnings : See Diagnostics"
+  write-DRMMAlert "Backup_Schedules : Warning : $($hashMsg) : See Diagnostics : $($finish)"
   write-DRMMDiag "$($script:diag)"
   exit 1
 } elseif (-not $script:blnWARN) {
-  write-DRMMAlert "Backup_Schedules : Completed Execution"
+  write-DRMMAlert "Backup_Schedules : Healthy : $($hashMsg) : $($finish)"
   write-DRMMDiag "$($script:diag)"
   exit 0
 }
