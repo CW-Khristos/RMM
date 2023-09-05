65e9be3302a77c6c9e7add49278b2e7538b8211a
CJ <khristos666@gmail.com>
2023-09-05 14:53:38 -0400

    Update hosts_0.1.1.ps1

--- Datto/hosts_0.1.1.ps1
+++ Datto/hosts_0.1.1.ps1
@@ -7,7 +7,7 @@
     Reports found changes back in diagnostic report
 
 .NOTES
-    Version        : 0.1.1 (30 March 2022)
+    Version        : 0.1.2 (05 September 2023)
     Creation Date  : 30 March 2022
     Purpose/Change : Monitors and reports on HOSTS file changes in the past 24 hours
     File Name      : hosts_0.1.1.ps1 
@@ -25,51 +25,79 @@
 #REGION ----- DECLARATIONS ----
   $script:intLN = 0
   $script:diag = $null
-  $script:arrCHG = [System.Collections.ArrayList]@()
+  $script:blnCHG = $false
+  $script:blnBAK = $false
   $script:arrHOST = [System.Collections.ArrayList]@()
 #ENDREGION ----- DECLARATIONS ----
 
 #REGION ----- FUNCTIONS ----
   function write-DRMMDiag ($messages) {
-    write-output  '<-Start Diagnostic->'
+    write-output "<-Start Diagnostic->"
     foreach ($Message in $Messages) {$Message}
-    write-output '<-End Diagnostic->'
+    write-output "<-End Diagnostic->"
   } ## write-DRMMDiag
   
   function write-DRMMAlert ($message) {
-    write-output '<-Start Result->'
+    write-output "<-Start Result->"
     write-output "Alert=$($message)"
-    write-output '<-End Result->'
+    write-output "<-End Result->"
   } ## write-DRRMAlert
 #ENDREGION ----- FUNCTIONS ----
 
 #------------
 #BEGIN SCRIPT
-if ((Get-ItemProperty -Path "$env:SystemRoot\System32\drivers\etc\hosts" -Name LastWriteTime).LastWriteTime -gt $((Get-Date).AddDays(-1))) {
+$strMod = $((Get-ItemProperty -Path "$env:SystemRoot\System32\drivers\etc\hosts" -Name LastWriteTime).LastWriteTime)
+if ($strMod -gt $((Get-Date).AddDays(-1))) {
   #COMPARE BACKUP TO CURRENT MODIFIED HOSTS FILE
-  Get-Content "C:\IT\hosts" | ForEach-Object {
+  Get-Content "C:\IT\hosts" -erroraction silentlycontinue | ForEach-Object {
     $script:arrHOST.add($_)
   }
   Get-Content "$env:SystemRoot\System32\drivers\etc\hosts" | ForEach-Object {
-    if ((($null -ne $_) -and ($_ -ne "")) -and ($_ -ne $script:arrHOST[$script:intLN])) {
-      #FLAG CHANGE
-      write-output "DETECTED CHANGE : $($_)"
-      $script:diag += "DETECTED CHANGE : $($_)`r`n"
-      $script:arrCHG.add($_)
+    if (($null -ne $_) -and ($_ -ne "")) {
+      if ($script:arrHOST) {
+        $script:blnBAK = $true
+          write-output "COMPARING : $($_) : $($script:arrHOST[$script:intLN])"
+          $script:diag += "COMPARING : $($_) : $($script:arrHOST[$script:intLN])`r`n"
+          if (($_ -match "\s") -and ($_ -ne $script:arrHOST[$script:intLN])) {
+            #FLAG CHANGE
+            $script:blnCHG = $true
+            write-output "DETECTED CHANGE : $($_)"
+            $script:diag += "DETECTED CHANGE : $($_)`r`n"
+          } else {
+            write-output "MATCHED - NO CHANGE"
+            $script:diag += "MATCHED - NO CHANGE`r`n"
+          }
+      }
     }
     $script:intLN += 1
   }
-  write-output $script:arrCHG
-  $strMod = $((Get-ItemProperty -Path "$env:SystemRoot\System32\drivers\etc\hosts" -Name LastWriteTime).LastWriteTime)
-  write-DRMMAlert "HOSTS modified within the last 24 hours. Last modification @ $($strMod)"
-  write-DRMMDiag "$($script:diag)"
-  Exit 1
+  if ($script:blnBAK -and $script:blnCHG) {
+    write-output "`r`nTEXT MODIFICATIONS MADE"
+    $script:diag += "`r`nTEXT MODIFICATIONS MADE"
+    write-DRMMAlert "HOSTS : Modified Within 24 hours. Last Modification @ $($strMod)"
+    write-DRMMDiag "$($script:diag)"
+    exit 1
+  } elseif (-not $script:blnCHG) {
+    if ($script:blnBAK) {
+        write-output "`r`nFILE MODIFIED; BUT NO TEXT MODIFICATIONS MADE"
+        $script:diag += "`r`nFILE MODIFIED; BUT NO TEXT MODIFICATIONS MADE"
+        write-DRMMAlert "HOSTS : Last Modification @ $($strMod) : No Text Changes"
+        write-DRMMDiag "$($script:diag)"
+        exit 0
+    } elseif (-not ($script:blnBAK)) {
+        write-output "`r`nFILE MODIFIED; BUT NO BACKUP FOR COMPARISON"
+        $script:diag += "`r`nFILE MODIFIED; BUT NO BACKUP FOR COMPARISON"
+        write-DRMMAlert "HOSTS : Last Modification @ $($strMod) : No Backup for Comparison"
+        write-DRMMDiag "$($script:diag)"
+        exit 1
+    }
+  }
 } else {
   #WRITE HOSTS BACKUP
-  $strMod = $((Get-ItemProperty -Path "$env:SystemRoot\System32\drivers\etc\hosts" -Name LastWriteTime).LastWriteTime)
   Copy-Item "$env:SystemRoot\System32\drivers\etc\hosts" -Destination "C:\IT\hosts"
-  write-DRMMAlert "HOSTS not modified since $($strMod)"
-  Exit 0
+  write-DRMMAlert "HOSTS : Not Modified Since $($strMod)"
+  write-DRMMDiag "$($script:diag)"
+  exit 0
 }
 #END SCRIPT
 #------------
