fc828c03b47b72327624e8dd04ca8f4764987052
CJ <khristos666@gmail.com>
2024-01-18 09:22:32 -0500

    Update hosts_0.1.1.ps1

--- Datto/hosts_0.1.1.ps1
+++ Datto/hosts_0.1.1.ps1
@@ -7,10 +7,10 @@
     Reports found changes back in diagnostic report
 
 .NOTES
-    Version        : 0.1.2 (05 September 2023)
+    Version        : 0.1.3 (17 January 2024)
     Creation Date  : 30 March 2022
     Purpose/Change : Monitors and reports on HOSTS file changes in the past 24 hours
-    File Name      : hosts_0.1.1.ps1 
+    File Name      : hosts_0.1.3.ps1 
     Author         : mat s., datto labs
     Modified       : Christopher Bledsoe - cbledsoe@ipmcomputers.com
 
@@ -22,18 +22,19 @@
 
 #>
 
-#REGION ----- DECLARATIONS ----
-  $script:intLN = 0
-  $script:diag = $null
-  $script:blnCHG = $false
-  $script:blnBAK = $false
-  $script:arrHOST = [System.Collections.ArrayList]@()
-#ENDREGION ----- DECLARATIONS ----
+#region ----- DECLARATIONS ----
+  $script:intLN       = 0
+  $script:intEntries  = 0
+  $script:diag        = $null
+  $script:blnCHG      = $false
+  $script:blnBAK      = $false
+  $script:arrHOST     = [System.Collections.ArrayList]@()
+#endregion ----- DECLARATIONS ----
 
-#REGION ----- FUNCTIONS ----
+#region ----- FUNCTIONS ----
   function write-DRMMDiag ($messages) {
     write-output "<-Start Diagnostic->"
-    foreach ($Message in $Messages) {$Message}
+    foreach ($message in $messages) {$message}
     write-output "<-End Diagnostic->"
   } ## write-DRMMDiag
   
@@ -42,59 +43,83 @@
     write-output "Alert=$($message)"
     write-output "<-End Result->"
   } ## write-DRMMAlert
-#ENDREGION ----- FUNCTIONS ----
+#endregion ----- FUNCTIONS ----
 
 #------------
 #BEGIN SCRIPT
-$strMod = $((Get-ItemProperty -Path "$env:SystemRoot\System32\drivers\etc\hosts" -Name LastWriteTime).LastWriteTime)
+$strMod = $((Get-ItemProperty -Path "$($env:SystemRoot)\System32\drivers\etc\hosts" -Name LastWriteTime).LastWriteTime)
 if ($strMod -gt $((Get-Date).AddDays(-1))) {
   #COMPARE BACKUP TO CURRENT MODIFIED HOSTS FILE
-  Get-Content "C:\IT\hosts" -erroraction silentlycontinue | ForEach-Object {
-    $script:arrHOST.add($_)
-  }
-  Get-Content "$env:SystemRoot\System32\drivers\etc\hosts" | ForEach-Object {
+  write-output "GETTING HOSTS BACKUP"
+  $script:diag += "GETTING HOSTS BACKUP`r`n"
+  Get-Content "C:\IT\hosts" -erroraction silentlycontinue | ForEach-Object {$script:arrHOST.add($_)}
+  write-output "HOSTS BACKUP ENTRIES : $($script:arrHOST.count)`r`n"
+  $script:diag += "HOSTS BACKUP ENTRIES : $($script:arrHOST.count)`r`n`r`n"
+  write-output "GETTING CURRENT HOSTS FILE"
+  $script:diag += "GETTING CURRENT HOSTS FILE`r`n"
+  Get-Content "$($env:SystemRoot)\System32\drivers\etc\hosts" | ForEach-Object {
     if (($null -ne $_) -and ($_ -ne "")) {
-      if ($script:arrHOST) {
+      if (-not ($script:arrHOST)) {
+        $script:blnBAK = $false
+      } elseif (($script:arrHOST) -and ($script:arrHOST[$script:intLN])) {
         $script:blnBAK = $true
-          write-output "COMPARING : $($_) : $($script:arrHOST[$script:intLN])"
-          $script:diag += "COMPARING : $($_) : $($script:arrHOST[$script:intLN])`r`n"
-          if (($_ -match "\s") -and ($_ -ne $script:arrHOST[$script:intLN])) {
-            #FLAG CHANGE
-            $script:blnCHG = $true
-            write-output "DETECTED CHANGE : $($_)"
-            $script:diag += "DETECTED CHANGE : $($_)`r`n"
-          } else {
-            write-output "MATCHED - NO CHANGE"
-            $script:diag += "MATCHED - NO CHANGE`r`n"
-          }
+        write-output "COMPARING : $($_) : $($script:arrHOST[$script:intLN])"
+        $script:diag += "COMPARING : $($_) : $($script:arrHOST[$script:intLN])`r`n"
+        if (($(($_).trim()) -match "\s") -and ($(($_).trim()) -ne $(($script:arrHOST[$script:intLN]).trim()))) {
+          #FLAG CHANGE
+          $script:blnCHG = $true
+          write-output "DETECTED CHANGE : $($_)"
+          $script:diag += "DETECTED CHANGE : $($_)`r`n"
+        } else {
+          write-output "MATCHED - NO CHANGE"
+          $script:diag += "MATCHED - NO CHANGE`r`n"
+        }
+      } else {
+        #FLAG CHANGE
+        $script:blnBAK = $true
+        $script:blnCHG = $true
+        write-output "END OF BACKUP : CONTENT ADDED :`r`n`tDETECTED CHANGE : $($_)"
+        $script:diag += "END OF BACKUP : CONTENT ADDED :`r`n`tDETECTED CHANGE : $($_)`r`n"
       }
     }
+    $script:intEntries += 1
     $script:intLN += 1
   }
   if ($script:blnBAK -and $script:blnCHG) {
-    write-output "`r`nTEXT MODIFICATIONS MADE"
-    $script:diag += "`r`nTEXT MODIFICATIONS MADE"
-    write-DRMMAlert "HOSTS : Modified Within 24 hours. Last Modification @ $($strMod)"
+    write-output "`r`nTEXT MODIFICATIONS MADE`r`n"
+    $script:diag += "`r`n`r`nTEXT MODIFICATIONS MADE`r`n"
+    write-DRMMAlert "HOSTS : Modified Within 24 hours : Last Modification @ $($strMod)"
     write-DRMMDiag "$($script:diag)"
     exit 1
   } elseif (-not $script:blnCHG) {
     if ($script:blnBAK) {
-        write-output "`r`nFILE MODIFIED; BUT NO TEXT MODIFICATIONS MADE"
-        $script:diag += "`r`nFILE MODIFIED; BUT NO TEXT MODIFICATIONS MADE"
-        write-DRMMAlert "HOSTS : Last Modification @ $($strMod) : No Text Changes"
-        write-DRMMDiag "$($script:diag)"
-        exit 0
+      write-output "FILE MODIFIED; BUT NO TEXT MODIFICATIONS MADE :"
+      $script:diag += "`r`nFILE MODIFIED; BUT NO TEXT MODIFICATIONS MADE :`r`n"
+      while ($script:intLN -le $script:arrHOST.count) {
+        if (($null -ne $($script:arrHOST[$script:intLN])) -and ($($script:arrHOST[$script:intLN]) -ne "")) {
+          write-output "`tCONTENT LIKELY REMOVED : $($script:arrHOST[$script:intLN])"
+          $script:diag += "`tCONTENT LIKELY REMOVED : $($script:arrHOST[$script:intLN])`r`n"
+        }
+        $script:intLN += 1
+      }
+      write-DRMMAlert "HOSTS : Last Modification @ $($strMod) : Content Removed / No Text Changes"
+      write-DRMMDiag "$($script:diag)"
+      exit 0
     } elseif (-not ($script:blnBAK)) {
-        write-output "`r`nFILE MODIFIED; BUT NO BACKUP FOR COMPARISON"
-        $script:diag += "`r`nFILE MODIFIED; BUT NO BACKUP FOR COMPARISON"
-        write-DRMMAlert "HOSTS : Last Modification @ $($strMod) : No Backup for Comparison"
-        write-DRMMDiag "$($script:diag)"
-        exit 0
+      write-output "`r`nFILE MODIFIED; BUT NO BACKUP FOR COMPARISON"
+      $script:diag += "`r`nFILE MODIFIED; BUT NO BACKUP FOR COMPARISON`r`n"
+      write-DRMMAlert "HOSTS : Last Modification @ $($strMod) : No Backup for Comparison"
+      write-DRMMDiag "$($script:diag)"
+      exit 0
     }
   }
 } else {
   #WRITE HOSTS BACKUP
-  Copy-Item "$env:SystemRoot\System32\drivers\etc\hosts" -Destination "C:\IT\hosts"
+  write-output "CREATING HOSTS BACKUP :"
+  $script:diag += "CREATING HOSTS BACKUP :`r`n"
+  Copy-Item "$($env:SystemRoot)\System32\drivers\etc\hosts" -Destination "C:\IT\hosts" -force
+  write-output "`tDONE"
+  $script:diag += "`tDone`r`n"
   write-DRMMAlert "HOSTS : Not Modified Since $($strMod)"
   write-DRMMDiag "$($script:diag)"
   exit 0
