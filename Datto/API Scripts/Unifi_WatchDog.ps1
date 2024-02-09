19509c94116886f951c000bd81a206159b4bae44
CJ <khristos666@gmail.com>
2024-02-09 12:06:50 -0500

    Update Unifi_WatchDog.ps1

--- Datto/API Scripts/Unifi_WatchDog.ps1
+++ Datto/API Scripts/Unifi_WatchDog.ps1
@@ -18,6 +18,18 @@
   $script:blnWARN           = $false
   $strLineSeparator         = "---------"
   $script:logPath           = "C:\IT\Log\Unifi_WatchDog"
+  #region######################## TLS Settings ###########################
+  [System.Net.ServicePointManager]::MaxServicePointIdleTime = 5000000
+  #[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType] 'Tls12'
+  [System.Net.ServicePointManager]::SecurityProtocol = (
+    [System.Net.SecurityProtocolType]::Ssl3 -bor 
+    [System.Net.SecurityProtocolType]::Ssl2 -bor 
+    [System.Net.SecurityProtocolType]::Tls13 -bor 
+    [System.Net.SecurityProtocolType]::Tls12 -bor 
+    [System.Net.SecurityProtocolType]::Tls11 -bor 
+    [System.Net.SecurityProtocolType]::Tls
+  )
+  #endregion
   ######################### Hudu Settings ###########################
   $script:huduCalls         = 0
   # Get a Hudu API Key from https://yourhududomain.com/admin/api_keys
